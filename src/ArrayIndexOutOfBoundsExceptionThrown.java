8a0b6c65543427c9a7dcde78f5e566cfcce988b8
Emma <emmayang78@gmail.com>
2017-07-14 14:46:55 +0800

    Added Out of Bounds

--- src/ArrayIndexOutOfBoundsExceptionThrown.java
+++ src/ArrayIndexOutOfBoundsExceptionThrown.java
@@ -5,7 +5,9 @@ public class ArrayIndexOutOfBoundsExceptionThrown {
 
     public static void main(String[] args) {
 
+        String[] words = {"hello", "world", "blah"};
 
+        System.out.println(words[5]);
 
     }
 
