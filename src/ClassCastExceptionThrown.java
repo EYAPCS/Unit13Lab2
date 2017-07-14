0455d56a45add40ca31496f4ede4a4348696af75
Emma <emmayang78@gmail.com>
2017-07-14 15:15:18 +0800

    Added ClassCastException

---
+++ src/ClassCastExceptionThrown.java
@@ -0,0 +1,11 @@
+/**
+ * Created by emma on 7/14/17.
+ */
+public class ClassCastExceptionThrown {
+
+    public static void main(String[] args) {
+        Object name = "Hi";
+        int nameInt = (int) name;
+    }
+
+}
